9e1b96ad833980b4c881851a1a28ae8b23ab304b
ranjanjs34 <Ranjan@Ranjan-Lapi.mzu>
2023-04-24 16:25:30 +0530

    first commit

---
+++ QC.sh
@@ -0,0 +1,2 @@
+fastqc /home/hp/Desktop/GC-148/Processed/*.fastq.gz -o /home/hp/Desktop/gc-148-qc/GC-148_out/Processed/
+fastqc /home/hp/Desktop/GC-148/Unprocessed/*.fastq.gz -o /home/hp/Desktop/gc-148-qc/GC-148_out/Unprocessed/
